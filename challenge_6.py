144ae43c6b0b54570e67aa75e74bed94882e5572
interngc <41315313+interngc@users.noreply.github.com>
2018-07-17 15:02:04 +0900

    my first commit

---
+++ challenge_6.py
@@ -0,0 +1,47 @@
+
+# challenge 1
+
+kamyu = "カミュ"
+print(kamyu[0])
+print(kamyu[1])
+print(kamyu[2])
+
+# challenge 2
+
+what = input("何を")
+where = input("どこに")
+
+print("私は昨日{}を書いて、{}に送った!".format(what, where))
+
+
+# challenge 3
+
+print("aldous Huxley was born in 1894.".capitalize())
+
+# chellenge 4
+
+wh_3 = "どこで？だれが？いつ？".split("？")
+print(wh_3)
+
+# challenge 5
+
+fox_list = ["The", "fox", "jumped", "over", "the", "fence", "."]
+print(" ".join(fox_list))
+
+# challenge 6
+print("A screaming comes across the sky.".replace("s","$"))
+
+# challenge 7
+print("Hemingway".index("m"))
+
+# challenge 8
+print("efawejioao\"fjaiewjoiewa\"fewiaj")
+
+# challenge 9
+print("three" + " three" + " three")
+print("three " * 3)
+
+# challenge 10
+april = "四月の晴れた寒い日で、時計がどれも十三時を打っていた。"
+print(april[:10])
+
